1209c7cbacf5b8c77610cfbf9dc7ec7094b7c1f7
ulr0 <ulr0xb@gmail.com>
2022-05-08 03:23:27 +0900

    add get survey detail(not done)

--- surveys/admin.py
+++ surveys/admin.py
@@ -1,8 +1,11 @@
+from collections import defaultdict
+
 from django.contrib import admin
 from django.utils.html import format_html
 from nested_admin import NestedTabularInline, NestedStackedInline, NestedModelAdmin
+from django.shortcuts import get_object_or_404, render
 
-from surveys.models import Survey, Question, Choice, Respondent
+from surveys.models import Survey, Question, Choice, Respondent, Answer
 
 class ChoiceInline(NestedTabularInline):
     model = Choice
@@ -39,7 +42,7 @@ class SurveyAdmin(NestedModelAdmin):
     inlines = [QuestionInline]
 
     def title_to_detail(self, obj):
-        return format_html(f"<a href='http://127.0.0.1:8000/admin/surveys/{obj.id}'>{obj.title}</a>")
+        return format_html(f"<a href='http://127.0.0.1:8000/admin/surveys/survey/{obj.id}'>{obj.title}</a>")
 
     def number_of_questions(self, obj):
         return Question.objects.filter(survey_id=obj.id).count()
@@ -49,5 +52,30 @@ class SurveyAdmin(NestedModelAdmin):
     
     def update(self, obj):
         return 'update'
+
+    def get_urls(self):
+        from django.urls import path
+        urls = super(SurveyAdmin, self).get_urls()
+        url = [
+            path('<int:survey_id>', self.admin_site.admin_view(self.get_survey_detail))
+        ]
+        return url + urls
+    
+    def get_survey_detail(self, request, survey_id):
+        survey = get_object_or_404(Survey, id=survey_id)
+        questions = Question.objects.prefetch_related('choice_set').filter(survey_id=survey_id)
+        number_of_respondents = Respondent.objects.filter(survey_id=survey_id).count()
+        answers = Answer.objects.select_related('respondent').filter(respondent__survey_id=survey_id)
+        
+        choice_count = defaultdict(int)
+        for answer in answers:
+            choice_count[answer.choice_id] += 1
+
+        return render(request, "templates/admin/survey_detail.html", {
+            'survey' : survey,
+            'questions' : questions,
+            'number_of_respondents' : number_of_respondents,
+            'choice_count' : choice_count
+        })
 
 admin.site.register(Survey, SurveyAdmin)
