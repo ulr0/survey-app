d3ad2a1f74a6818cc986dea733b59cab5ec2a317
ulr0 <ulr0xb@gmail.com>
2022-05-08 22:35:52 +0900

    add get survey detail - add link to survey page - add percent of choice

---
+++ surveys/templatetags/surveys_extras.py
@@ -0,0 +1,19 @@
+from django import template
+
+register = template.Library()
+
+def get_count(dict, key):
+    return dict[key]
+
+def get_percent(num1, num2):
+    try: 
+        percent = (num1 / num2) * 100
+
+        return round(percent, 2)
+    
+    except:
+        return None
+
+
+register.filter('get_count', get_count)
+register.filter('get_percent', get_percent)
