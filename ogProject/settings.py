a34c0342b45be2955576383418fee1f6b2c499d6
ulr0 <ulr0xb@gmail.com>
2022-05-09 00:22:54 +0900

    deployment settings

--- ogProject/settings.py
+++ ogProject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = env.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*', env.PUBLIC_IP]
+ALLOWED_HOSTS = ['*', env.PUBLIC_IP_1, env.PUBLIC_IP_2]
 
 
 # Application definition
